b3146f7b04bca7459ec23f46e800203a2a94a198
Cachopa007 <00001107671863SP@al.educacao.sp.gov.br>
2025-09-02 10:08:50 -0300

    a

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');  
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
 
-    aumentarFonteBotao.addEventListener('click', function(){
+    aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
     })
 
      diminuirFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-
     })
-    
+
 })
